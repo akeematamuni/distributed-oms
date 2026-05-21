481c03e73df1c75a45e5970b2e69cc88227f1f4d
akeematamuni <amuni.engr@gmail.com>
2026-05-21 13:24:12 +0100

    revert: removed placeholder files for kernel

--- libs/shared/kernel/src/lib/shared-kernel.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sharedKernel } from './shared-kernel';
-
-describe('sharedKernel', () => {
-    it('should work', () => {
-        expect(sharedKernel()).toEqual('shared-kernel');
-    });
-});
