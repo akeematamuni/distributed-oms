11eca7b54fa8396d27e2286f7bffab226bbc9143
akeematamuni <amuni.engr@gmail.com>
2026-06-02 14:54:42 +0100

    refactor: removed irrelevant id classes

--- libs/order/domain/src/lib/value-objects/customer-id.vo.ts
+++
@@ -1,8 +0,0 @@
-import { UniqueId } from '@doms/shared/kernel';
-
-export class CustomerId extends UniqueId {
-    static create(value: string): CustomerId {
-        CustomerId.validate(value);
-        return new CustomerId({ value });
-    }
-}
--- libs/order/domain/src/lib/value-objects/order-id.vo.ts
+++
@@ -1,8 +0,0 @@
-import { UniqueId } from '@doms/shared/kernel';
-
-export class OrderId extends UniqueId {
-    static create(value: string): OrderId {
-        OrderId.validate(value);
-        return new OrderId({ value });
-    }
-}
