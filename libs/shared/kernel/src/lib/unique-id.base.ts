2008877ef62b04e26ef992ac7f2f74da260124b2
akeematamuni <amuni.engr@gmail.com>
2026-06-02 14:01:34 +0100

    refactor: base id class refactor to have create and fromExisting

--- libs/shared/kernel/src/lib/unique-id.base.ts
+++ libs/shared/kernel/src/lib/unique-id.base.ts
@@ -1,13 +1,25 @@
+import { randomUUID } from 'node:crypto';
 import { ValueObjectBase } from './value-object.base';
 
-export abstract class UniqueId extends ValueObjectBase<{ value: string }> {
-    protected static validate(value: string): void {
+export class UniqueId extends ValueObjectBase<{ value: string }> {
+    private static validateRandomUUID(value: string): void {
         const UUID_V4_REGEX =
             /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
         if (!UUID_V4_REGEX.test(value)) {
-            throw new Error(`ID must be a valid UUID v4. You provided: ${value}`);
+            throw new Error(`ID must be a valid UUID v4. Got: ${value}`);
         }
+    }
+
+    static create(): UniqueId {
+        const value = randomUUID();
+        this.validateRandomUUID(value);
+        return new UniqueId({ value });
+    }
+
+    static fromExisting(value: string): UniqueId {
+        this.validateRandomUUID(value);
+        return new UniqueId({ value });
     }
 
     get value(): string {
