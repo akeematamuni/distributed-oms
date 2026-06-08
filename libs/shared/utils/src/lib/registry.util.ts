6996d0dd477547e5628dcc59643ac2692ceb7d65
akeematamuni <amuni.engr@gmail.com>
2026-06-08 15:03:12 +0100

    feat: global registry for custom errors mapping

---
+++ libs/shared/utils/src/lib/registry.util.ts
@@ -0,0 +1,15 @@
+import { HttpStatus, Type } from '@nestjs/common';
+
+export interface ErrorMapping {
+    exception: Type<unknown>;
+    statusCode: HttpStatus;
+}
+
+// Leverage inversion of control pattern to prevent circular dependency
+export class GlobalErrorRegistry {
+    public static readonly errorMappings: ErrorMapping[] = [];
+
+    public static addErrorMappings(mappings: ErrorMapping[]) {
+        this.errorMappings.push(...mappings);
+    }
+}
