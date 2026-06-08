ab8b0746afb1bfa0a670d866bb82bddccebb0885
akeematamuni <amuni.engr@gmail.com>
2026-06-08 14:32:21 +0100

    chore: module export for order line

--- libs/order/domain/src/index.ts
+++ libs/order/domain/src/index.ts
@@ -1,1 +1,11 @@
+// Value Objects
 export * from './lib/value-objects/money.vo';
+export * from './lib/value-objects/order-status.vo';
+export * from './lib/value-objects/address.vo';
+
+// Entities
+export * from './lib/entities/order-line.entity';
+
+// Exceptions
+
+// Aggreagtes
