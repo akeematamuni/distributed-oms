8dafde93f6a160575b26672cd4b9bd5eb0fa2e07
akeematamuni <amuni.engr@gmail.com>
2026-06-17 15:21:37 +0100

    feat: order creation command and barrel exports

--- libs/order/application/src/index.ts
+++ libs/order/application/src/index.ts
@@ -1,1 +1,20 @@
 export * from './lib/order-application.module';
+
+// Types
+export * from './lib/types/address.type';
+export * from './lib/types/create-order.type';
+export * from './lib/types/order-line.type';
+
+// DTOs
+export * from './lib/dtos/address.dto';
+export * from './lib/dtos/create-order.dto';
+export * from './lib/dtos/order-line.dto';
+export * from './lib/dtos/order-response.dto';
+
+// Commands
+export * from './lib/commands/create-order.command';
+export * from './lib/commands/create-order.handler';
+
+// Queries
+
+// Sagas
---
+++ libs/order/application/src/lib/commands/create-order.command.ts
@@ -0,0 +1,15 @@
+import { CommandBase } from '@doms/shared/kernel';
+import { AddressInput } from '../types/address.type';
+import { OrderLineInput } from '../types/order-line.type';
+
+export class CreateOrderCommand extends CommandBase {
+    constructor(
+        public readonly customerId: string,
+        public readonly channel: string,
+        public readonly shippingAddress: AddressInput,
+        public readonly lines: Array<OrderLineInput>,
+        correlationId?: string,
+    ) {
+        super(correlationId);
+    }
+}
