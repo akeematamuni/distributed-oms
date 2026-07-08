56108ac16aa9376ff69966979c59d0396f600bea
akeematamuni <amuni.engr@gmail.com>
2026-07-08 16:00:35 +0100

    feat: preliminary wiring of order-capture service

--- apps/order-capture/src/app/app.module.ts
+++ apps/order-capture/src/app/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SharedUtilsModule } from '@doms/shared/utils';
+import { SharedRedisModule } from '@doms/shared/redis';
+
+import {
+    OrderTypeOrmEntity,
+    OrderLineTypeOrmEntity,
+    OrderOutboxTypeOrmEntity,
+} from '@doms/order/infrastructure';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        SharedUtilsModule,
+        SharedRedisModule,
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                url: config.get('ORDER_DB_URL'),
+                entities: [OrderTypeOrmEntity, OrderLineTypeOrmEntity, OrderOutboxTypeOrmEntity],
+                synchronize: false,
+            }),
+        }),
+        TypeOrmModule.forFeature([
+            OrderTypeOrmEntity,
+            OrderLineTypeOrmEntity,
+            OrderOutboxTypeOrmEntity,
+        ]),
+    ],
+    providers: [
+        AppService,
+        // Order domain and infrastructure layer imports,
+        // Order application layer imports,
+    ],
     controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
