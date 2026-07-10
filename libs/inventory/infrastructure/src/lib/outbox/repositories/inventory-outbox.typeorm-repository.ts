7f2da08814e2b6fd42cf78d9cead54ae62dc18bf
akeematamuni <amuni.engr@gmail.com>
2026-07-10 13:22:58 +0100

    feat: updated inventory outbox to use skip locked

--- libs/inventory/infrastructure/src/lib/outbox/repositories/inventory-outbox.typeorm-repository.ts
+++ libs/inventory/infrastructure/src/lib/outbox/repositories/inventory-outbox.typeorm-repository.ts
@@ -8,7 +8,7 @@ import { InventoryOutboxTypeOrmEntity } from '../entities/inventory-outbox.typeo
 export class InventoryOutboxTypeOrmRepository implements IOutboxRepositoryPort {
     constructor(
         @InjectRepository(InventoryOutboxTypeOrmEntity)
-        private readonly repository: Repository<InventoryOutboxTypeOrmEntity>,
+        private readonly orderOutboxRepository: Repository<InventoryOutboxTypeOrmEntity>,
     ) {}
 
     private getRepository(queryRunner?: unknown): Repository<InventoryOutboxTypeOrmEntity> {
@@ -17,32 +17,44 @@ export class InventoryOutboxTypeOrmRepository implements IOutboxRepositoryPort {
             return qr.manager.getRepository(InventoryOutboxTypeOrmEntity);
         }
 
-        return this.repository;
+        return this.orderOutboxRepository;
     }
 
     async save(
         record: Omit<OutboxRecord, 'createdAt' | 'publishedAt' | 'retryCount'>,
-        queryRunner?: unknown,
+        queryRunner: unknown,
     ): Promise<void> {
         await this.getRepository(queryRunner).save(record);
     }
 
-    async findPending(limit: number, queryRunner?: unknown): Promise<OutboxRecord[]> {
-        return await this.getRepository(queryRunner).find({
-            where: { status: OutboxStatus.PENDING },
-            order: { createdAt: 'ASC' },
-            take: limit,
-        });
+    // SKIP LOCKED must be used to avoid collision
+    async findPending(limit: number, queryRunner: unknown): Promise<OutboxRecord[]> {
+        // return await this.getRepository(queryRunner).find({
+        //     where: { status: OutboxStatus.PENDING },
+        //     order: { createdAt: 'ASC' },
+        //     take: limit,
+        // });
+
+        const qr = queryRunner as QueryRunner;
+
+        return qr.manager.query(
+            `SELECT * FROM inventory_outboxes
+            WHERE status = 'PENDING'
+            ORDER BY created_at ASC
+            LIMIT $1
+            FOR UPDATE SKIP LOCKED`,
+            [limit],
+        );
     }
 
-    async markPublished(id: string, queryRunner?: unknown): Promise<void> {
+    async markPublished(id: string, queryRunner: unknown): Promise<void> {
         await this.getRepository(queryRunner).update(id, {
             status: OutboxStatus.PUBLISHED,
             publishedAt: new Date(),
         });
     }
 
-    async markFailed(id: string, queryRunner?: unknown): Promise<void> {
+    async markFailed(id: string, queryRunner: unknown): Promise<void> {
         await this.getRepository(queryRunner).update(id, {
             status: OutboxStatus.FAILED,
             retryCount: () => 'retry_count + 1',
