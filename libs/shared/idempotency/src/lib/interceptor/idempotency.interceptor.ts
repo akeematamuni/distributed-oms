c44aa1318722948dbc38bfc35f8d9fbfbadac573
akeematamuni <amuni.engr@gmail.com>
2026-07-14 10:37:13 +0100

    feat: updated idempotency interceptor to include valid uuid and create a unique caching key

--- libs/shared/idempotency/src/lib/interceptor/idempotency.interceptor.ts
+++ libs/shared/idempotency/src/lib/interceptor/idempotency.interceptor.ts
@@ -9,11 +9,15 @@ import {
     HttpStatus,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, of, concatMap } from 'rxjs';
 import { IDEMPOTENCY_STORE, IIdempotencyStorePort } from '../port/idempotency.store.port';
 
 const IDEMPOTENCY_KEY_HEADER = 'idempotency-key';
 
+/**
+ * Leverage customerId and idempotency key as unique key for caching responses
+ * Upgrade to a more robust authenticated approach later (e.g JWT validated IDs)
+ */
 @Injectable()
 export class IdempotencyInterceptor implements NestInterceptor {
     private readonly logger = new Logger(IdempotencyInterceptor.name);
@@ -24,24 +28,38 @@ export class IdempotencyInterceptor implements NestInterceptor {
     ) {}
 
     async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<unknown>> {
+        const UUID_V4_REGEX =
+            /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
         const request = context.switchToHttp().getRequest();
 
         const key = request.headers[IDEMPOTENCY_KEY_HEADER];
         if (!key)
             throw new HttpException('Idempotency-Key header is required', HttpStatus.BAD_REQUEST);
 
-        request.correlationId = key;
+        if (!UUID_V4_REGEX.test(key))
+            throw new HttpException(
+                'Idempotency key must be a valid UUID v4 string',
+                HttpStatus.BAD_REQUEST,
+            );
 
-        const cached = await this.store.get(key);
+        const customerId = request.body?.customerId;
+        if (!customerId)
+            throw new HttpException('Customer ID is required in payload', HttpStatus.BAD_REQUEST);
+
+        // Create a unique key
+        const scopedKey = `${key}:${customerId}`;
+
+        const cached = await this.store.get(scopedKey);
         if (cached) {
-            this.logger.log(`Idempotency hit for key: ${key}`);
+            this.logger.log(`Idempotency hit for key: ${scopedKey}`);
             return of(cached);
         }
 
         const ttlSeconds = this.config.get('TTL_SECONDS', 86400);
         return next.handle().pipe(
-            tap(async (response) => {
-                await this.store.set(key, response, ttlSeconds);
+            concatMap(async (response) => {
+                await this.store.set(scopedKey, response, ttlSeconds);
             }),
         );
     }
